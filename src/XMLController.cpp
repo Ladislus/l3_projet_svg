07450878e4f97d41fb0735ed4f71cac4dfef52a5
Ladislus <walcak.ladislas@gmail.com>
2020-03-11 08:25:17 +0100

    [FIX] Changed absolute path of the image to a relative path from the root directory

--- src/XMLController.cpp
+++ src/XMLController.cpp
@@ -8,7 +8,7 @@ XMLController::XMLController() {
 
     /* Load the svg filename */
     //TODO : relative path
-    this->_svg_data.LoadFile("/home/ladislus/Git/drieux_joffroy_walcak_mercier/Resources/Image Samples/atom.svg");
+    this->_svg_data.LoadFile("Resources/Image Samples/atom.svg");
 
     XMLPrinter printer;
     GError* errors;

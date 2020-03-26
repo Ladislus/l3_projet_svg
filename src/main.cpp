26be8dfa050ba49791636d1a6c5f7f6dad6b99bc
Ladislus <walcak.ladislas@gmail.com>
2020-03-26 02:20:41 +0100

    [MOD] Changed port convertion from string to int with a method that check errors

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,12 @@ int main(int argc, char *argv[]) {
 
     unsigned short int port = 6789;
     if (argc >= 2) {
-        port = atoi(argv[1]);
+        char *error;
+        port = strtol(argv[1], &error, strlen(argv[1]));
+        if (*error) {
+            std::cout << "Bad port provided, using default port 6789" << std::endl;
+            port = 6789;
+        }
     } else {
         std::cout << "No port provided, using default port 6789" << std::endl;
     }
